7591f8ca7311477c5511c4a0781f7103601d3d19
Ryan Brosius <ryanbrosius00@gmail.com>
2024-04-21 01:42:13 -0400

    Forgot to commit flask

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask import request
 from flask_cors import CORS
 import time
+from yamlDB import yamlDB
 
 app = flask.Flask(__name__)
 app.secret_key = 'hatsune_miku'
@@ -20,9 +21,26 @@ def test():
 @app.route('/sendTranscript', methods=['POST'])
 def sendTranscript():
     data = request.json
+    print(data)
     
     return jsonify({'user'    : 'system',
                     'content' : 'lol this is an example'})
+
+@app.route('/sendJournal', methods=['POST'])
+def sendJournal():
+    data = request.json
+    entry = data['content']
+
+    db = yamlDB()
+    db.inject(data)
+
+    return "success!"
+
+@app.route('/getJournals', methods=['GET'])
+def getJournals():
+    db = yamlDB()
+    data = db.getAll()
+    return data
 
 
 
